7d89cad025391ba23b32cafd7686d784d90d6648
Nuradiyana <me@nooradiana.com>
2024-02-09 20:47:19 +0700

    Tambah `SnakeCaseMapper`

--- src/Http/Integrations/Inputs/WebhookUrlInput.php
+++ src/Http/Integrations/Inputs/WebhookUrlInput.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace FromHome\Moota\Http\Integrations\Inputs;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 use Spatie\LaravelData\Attributes\Validation\In;
 use Spatie\LaravelData\Attributes\Validation\Url;
 
+#[MapName(SnakeCaseMapper::class)]
 final class WebhookUrlInput extends Data
 {
     public function __construct(
