9190351ca3e40216e841b282f05664cf74edba0c
Nuradiyana <me@nooradiana.com>
2025-02-28 17:53:39 +0700

    Remove `saloonphp/laravel-http-sender`

--- src/MootaServiceProvider.php
+++ src/MootaServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace FromHome\Moota;
 
-use Saloon\HttpSender\HttpSender;
 use Spatie\LaravelPackageTools\Package;
 use FromHome\Moota\Http\Integrations\MootaConnector;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -13,8 +12,6 @@ final class MootaServiceProvider extends PackageServiceProvider
 {
     public function registeringPackage(): void
     {
-        $this->app->singleton(HttpSender::class, fn () => new HttpSender);
-
         $this->app->singleton(MootaConnector::class, function (): MootaConnector {
             return new MootaConnector(
                 \config('services.moota.token', ''),
